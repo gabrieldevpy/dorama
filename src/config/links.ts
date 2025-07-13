ab17a716a76122d1b4d13aa79ad47345e1c62d42
Gabriel <gsommer782@gmail.com>
2025-07-13 17:45:25 +0000

    https://doramainde.netlify.app/

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,4 +1,4 @@
 // Use este arquivo para gerenciar todos os links importantes do seu site.
 // Altere o link de compra aqui uma única vez e ele será atualizado em toda a página.
 
-export const purchaseLink = 'https://seu-link-de-compra.com'; // <<< COLOQUE SEU LINK DE AFILIADO AQUI
+export const purchaseLink = 'https://doramainde.netlify.app/'; // <<< COLOQUE SEU LINK DE AFILIADO AQUI
