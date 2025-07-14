636a2cd9cf539b2d5bdc09b20e3e78e40d2e2e84
Gabriel <gsommer782@gmail.com>
2025-07-14 23:29:51 +0000

    ele deve ser redirecionado para o https://pay.kirvano.com/d1c8417b-3713-

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,4 +1,4 @@
 // Use este arquivo para gerenciar todos os links importantes do seu site.
 // Altere o link de compra aqui uma única vez e ele será atualizado em toda a página.
 
-export const purchaseLink = 'https://pay.kirvano.com/d1c8417b-3713-4d07-bdc1-0fe21eb99f4a'; // <<< COLOQUE SEU LINK DE AFILIADO AQUI
+export const purchaseLink = 'https://pay.kirvano.com/d1c8417b-3713-4d07-bdc1-0fe21eb99f4a';
